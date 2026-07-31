0cf86f86504afee3e994186fcc4d5239953e9069
Raul <max_raul_santos@hotmail.com>
2026-07-31 11:20:49 -0300

    Created settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+# Classe para settings
+
+class Settings:
+    """A class to store all settings for Alien invasion"""
+
+    def __init__(self):
+        """initilize settings"""
+        self.SW = 1200      # Screen Width - altura
+        self.SH = 800       # Screen Hight - largura
+        self.BG_COLOR = (230,230,230)
